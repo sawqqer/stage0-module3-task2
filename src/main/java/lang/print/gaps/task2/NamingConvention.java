926fe9b60415cae5c9807a6ea5d71d87f06ba474
Arman <gcom0733@gmail.com>
2023-02-13 22:51:05 +0600

    fixed errors

--- src/main/java/lang/print/gaps/task2/NamingConvention.java
+++ src/main/java/lang/print/gaps/task2/NamingConvention.java
@@ -8,8 +8,8 @@ public class NamingConvention {
         int variable_three = 3;
         final int VARIABLE_FOUR = 4;
         final int VARIABLE_FIVE = 5;
-        int VariableSix = 6;
+        int variableSix = 6;
         int variableSeven = 7;
-        int VariableEight = 8;
+        int variableEight = 8;
     }
 }
