cf9a8055ed4036e8d8da867205697b23f5fe0ada
Arman <gcom0733@gmail.com>
2023-02-13 22:36:22 +0600

    fixed errors

--- src/main/java/lang/print/gaps/task2/NamingConvention.java
+++ src/main/java/lang/print/gaps/task2/NamingConvention.java
@@ -2,14 +2,14 @@ package lang.print.gaps.task2;
 
 public class NamingConvention {
     public static void main(String[] args) {
-        int _variable = 10;
-        int variable_One = 1;
-        int variableTwo = 2;
+        int variable = 10;
+        int variable_one = 1;
+        int variable_two = 2;
         int variable_three = 3;
-        final int VARIABLEFOUR = 4;
-        final int VARIABLEfIVE = 5;
-        int VariableSix = 6;
-        int variableseven = 7;
-        int VariableEight = 8;
+        final int VARIABLE_FOUR = 4;
+        final int VARIABLE_FIVE = 5;
+        int Variable_six = 6;
+        int variable_seven = 7;
+        int Variable_eight = 8;
     }
 }
